23927349700f2465d41e7e2f60504dd5c203b409
Samesh <samesh.lakhotia@gmail.com>
2019-04-09 19:33:33 +0530

    Add basic templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Samesh Lakhotia',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 9,2019'
+    },
+    {
+        'author': 'Harley Quinn',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 10,2019'
+    }
+]
+
+
 # Create your views here.
 
-
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+        'title': "Home"
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': "About Us"})
